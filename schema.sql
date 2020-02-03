8303a0d246cd4cfd8b55d6280e3bd0ede57e9e4c
travis-john <travis.wl.john@gmail.com>
2020-02-03 09:27:07 -0500

    finalizing schema

--- schema.sql
+++ schema.sql
@@ -4,34 +4,34 @@ CREATE DATABASE ems_db;
 
 USE ems_db;
 
-CREATE TABLE department(
+CREATE TABLE departments(
     d_id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 
-CREATE TABLE role(
+CREATE TABLE roles(
     r_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (8,2) NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(d_id)
+    FOREIGN KEY (department_id) REFERENCES departments(d_id)
 );
 
 
-CREATE TABLE employee(
+CREATE TABLE employees(
     e_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(r_id),
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employee(e_id)
+    FOREIGN KEY (manager_id) REFERENCES employees(e_id)
 );
 
 
-SELECT*FROM department;
+SELECT*FROM departments;
 
-SELECT*FROM rolee;
+SELECT*FROM roles;
 
-SELECT*FROM employee;
+SELECT*FROM employees;
