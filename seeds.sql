762ae144b6b48c831a96004aece5e5077aee3eb8
travis-john <travis.wl.john@gmail.com>
2020-02-03 16:00:10 -0500

    adding seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO departments(d_id, department_name)
+VALUES
+(1, 'SALES'),
+(2, 'ENGINEERING'),
+(3, 'FINANCE'),
+(4, 'LEGAL');
+
+INSERT INTO roles(r_id, title, salary, department_id)
+VALUES
+(1, 'Sales Lead', 100000, 1),
+(2, 'Salesperson', 60000, 1),
+(3, 'Software Engineer', 120000, 2),
+(4, 'Lead Software Engineer', 150000, 2),
+(5, 'Accountant', 125000, 3),
+(6, 'Lawyer', 150000, 4),
+(7, 'Legal Team Lead', 180000, 4);
+
+INSERT INTO employees(e_id, first_name, last_name, role_id, manager_id)
+VALUES
+(1, 'John', 'Doe', 1, NULL),
+(2, 'Mike', 'Chan', 2, 1),
+(3, 'Ashley', 'Rodriguez', 4, NULL),
+(4, 'Kevin', 'Tupik', 3, 3),
+(5, 'Malia', 'Brown', 5, NULL),
+(6, 'Sarah', 'Lourd', 7, NULL),
+(7, 'Tom', 'Allen', 7, NULL );
