1086dccd82d6299c2cd313796ae8ca38b4d213c8
travis-john <travis.wl.john@gmail.com>
2020-02-05 17:41:11 -0500

    schema and seeds working as expected with view function

--- seeds.sql
+++ seeds.sql
@@ -1,28 +1,28 @@
-INSERT INTO departments(d_id, department_name)
+INSERT INTO departments(department_name)
 VALUES
-(1, 'SALES'),
-(2, 'ENGINEERING'),
-(3, 'FINANCE'),
-(4, 'LEGAL');
+('SALES'),
+('ENGINEERING'),
+('FINANCE'),
+('LEGAL');
 
-INSERT INTO roles(r_id, title, salary, department_id)
+INSERT INTO roles(title, salary, department_id)
 VALUES
-(1, 'Sales Lead', 100000, 1),
-(2, 'Salesperson', 60000, 1),
-(3, 'Software Engineer', 120000, 2),
-(4, 'Lead Software Engineer', 150000, 2),
-(5, 'Accountant', 125000, 3),
-(6, 'Lead Accoutant', 150000, 3),
-(7, 'Lawyer', 150000, 4),
-(8, 'Legal Team Lead', 180000, 4);
+('Sales Lead', 100000, 1),
+('Salesperson', 60000, 1),
+('Software Engineer', 120000, 2),
+('Lead Software Engineer', 150000, 2),
+('Accountant', 125000, 3),
+('Lead Accoutant', 150000, 3),
+('Lawyer', 150000, 4),
+('Legal Team Lead', 180000, 4);
 
-INSERT INTO employees(e_id, first_name, last_name, role_id, manager_id)
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
 VALUES
-(1, 'John', 'Doe', 1, NULL),
-(2, 'Mike', 'Chan', 2, 1),
-(3, 'Ashley', 'Rodriguez', 4, NULL),
-(4, 'Kevin', 'Tupik', 3, 3),
-(5, 'Malia', 'Brown', 5, 6),
-(6, 'Kevin', 'Malone',6, NULL ),
-(7, 'Sarah', 'Lourd', 8, NULL),
-(8, 'Tom', 'Allen', 7, 7 );
+('John', 'Doe', 1, NULL),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, NULL),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, NULL),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, NULL),
+('Tom', 'Allen', 8, 7);
